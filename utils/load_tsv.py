b8139ba08551cbd7a27970957a67061a9e492c8b
luckyluc99 <167755360+luckyluc99@users.noreply.github.com>
2024-06-22 18:59:14 +0200

    Add gitignore and function for loading datasets

---
+++ utils/load_tsv.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+def load_from_tsvfile(file_path):
+    df = pd.read_csv(file_path, sep="\t", index_col=False, header=None)
+    y = df.iloc[:, 0].to_numpy()
+    X = np.expand_dims(df.iloc[:, 1:].to_numpy(), axis=1)
+    df = df.to_numpy()
+    return X, y
